0cf45e0d28bcd007f7006a69452cbf6b9319c33b
Sahaj-Bamba <sahajbamba1999@gmail.com>
2018-10-25 00:45:51 +0530

    Started working on owner parts

--- index.php
+++ index.php
@@ -23,6 +23,22 @@
 	  <script type="text/javascript" src="javascript/index.js"></script>
 	</head>
 	
+	<style type="text/css">
+	.carousel-inner img {
+		/*width: 100%;*/
+		margin: auto;
+		min-height:200px;
+		max-height: 600px;
+		max-width: 700px;
+	}
+	@media (max-width: 600px) {
+		.carousel-caption {
+			display: none; 
+		}
+	}
+
+	</style>
+
 	<body>
 
 		<nav class="navbar navbar-inverse navbar-fixed-top">
@@ -67,6 +83,54 @@
 		  </div>
 		</nav>
 
+
+		<div class="jumbotron">
+			<div class="container text-center">
+				<h1> Bamba Kirana Store </h1>      
+				<strong><h2 class="mark"> You Want it ? We got it. </h2></strong>
+			</div>
+		</div>
+
+		
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		    <!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+			</ol>
+
+		    <!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="Image/Site/1.jpeg" alt="Image">
+					<div class="carousel-caption">
+						<h3>Our Products $</h3>
+						<!--	<p>Money Money.</p>	-->
+					</div>      
+				</div>
+
+				<div class="item">
+					<img src="Image/Site/2.jpeg" alt="Image">
+					<div class="carousel-caption">
+						<h3>More Products $</h3>
+						<!--p>Lorem ipsum...</p-->
+					</div>      
+				</div>
+			</div>
+
+		    <!-- Left and right controls -->
+		    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		    </a>
+		    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		    </a>
+		</div>
+		  
+
 	</body>
+
 
 </html>
--- test.php
+++ test.php
@@ -7,6 +7,25 @@ print_r($_SESSION["gamer"]);
 
 //echo $_SESSION["ID"];
 
+if(strcmp($_SESSION["gamer"]["type"], "owner") != 0 ){
+	echo "hi";
+}
+else {
+	echo $_SESSION["gamer"]["type"];
+}
 
+if(!(isset($_SESSION["gamer"]))){
+	echo "hello";
+}
+else{
+	echo "die3";
+}
+
+if (((!(isset($_SESSION["gamer"])))||(strcmp($_SESSION["gamer"]["type"], "owner") != 0 ))) {
+	echo "die 4";
+}
+else{
+	echo "live";
+}
 ?>
 
