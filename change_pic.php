993e317a3042ac641ef01def2ee8517ef2e3afd4
Sahaj-Bamba <sahajbamba1999@gmail.com>
2018-10-27 12:58:16 +0530

    updated the change pic part

--- change_pic.php
+++ change_pic.php
@@ -1,18 +1,40 @@
+
+
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Bamba Kirana Store</title>
-</head>
-<body>
+	<head>
+		
+		<meta charset="UTF-8">
+		<title>Bamba Kirana Store</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		
+		<link rel="icon" type="image/x-icon" href="favicon.ico">
+		<link rel="stylesheet" type="text/css" href="style/login.css">
+			
+	</head>
+	<body style="background: url('Image/background.jpg');">
+		<div class="box">
+			<h2>Change Profile Picture</h2>
+			
+			<form enctype="multipart/form-data" method="POST" action="change_pic_.php" autocomplete="off" >
 
-<form enctype="multipart/form-data" action="change_pic_.php" method="POST">
-    <!--<input type="hidden" name="MAX_FILE_SIZE" value="512000000000" />-->
-    Send this file: <input type="file" name="pic"/>
-    <input type="submit" value="Send File" name="save" />
-</form>
-
-
-
-
-</body>
+				<div class="inputbox">
+					<input type="file" name="pic" required />
+					<label>Image</lable>
+				</div>
+				<center>
+					<input type="submit" name="submit" value="Submit">
+				</center>
+			</form>
+			<br>
+			<center>
+				<a href="index.php"><button> Go Back </button> </a>
+			</center>
+		</div>
+	</body>
 </html>
